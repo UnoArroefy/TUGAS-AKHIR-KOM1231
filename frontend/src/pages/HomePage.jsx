c2dc97d646d2f69d5e060dcbf5ff8215f03b81ef
ozilazhiim <142593766+ozilazhiim@users.noreply.github.com>
2024-05-17 08:22:04 +0700

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,22 @@
 import { Button } from '@/components/ui/button'
-import { useEffect } from 'react'
-import { useAuth } from '@/components/AuthProvider'
-import { jwtDecode } from 'jwt-decode'
-import { Layout } from '@/components/ui/layout'
+import { useState } from "react";
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { useAuth } from '@/components/AuthProvider';
+import { jwtDecode } from 'jwt-decode';
+import { Layout } from '@/components/ui/layout';
+
 
 export const HomePage = () => {
     useEffect(() => {
@@ -21,4 +35,23 @@ export const HomePage = () => {
             </div>
         </Layout>
     )
+}
+const Calendar = () => {
+    const [currentEvents, setCurrentEvents] = useState([]);
+  
+    const handleDateClick = (selected) => {
+      const title = prompt("Please enter a new title for your event");
+      const calendarApi = selected.view.calendar;
+      calendarApi.unselect();
+  
+      if (title) {
+        calendarApi.addEvent({
+          id: `${selected.dateStr}-${title}`,
+          title,
+          start: selected.startStr,
+          end: selected.endStr,
+          allDay: selected.allDay,
+        });
+      }
+    };
 }
